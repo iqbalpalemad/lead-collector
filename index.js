e9adb081eba405ea4fdbae7b57f3c8b1869f8108
Muhammed Iqbal <muhammed.iqbalea@zoominfo.com>
2025-06-25 15:38:08 +0530

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/mongo');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
